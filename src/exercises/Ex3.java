a3d9f2f311cf8b73d312ee47dc138c1705799c1e
3igenKrak3n <3igen3ggy@gmail.com>
2022-02-22 22:01:05 +0100

    fixed Ex3 fixed back isPrime function

--- src/exercises/Ex3.java
+++ src/exercises/Ex3.java
@@ -3,12 +3,18 @@ package exercises;
 public class Ex3{
     public long solve() {
         long num = 600851475143L;
+        long max = 0L;
         long start = num / 2;
-        for (long i = start; i > 2; i--) {
-            if (num % i == 0 && utilities.isPrime.primeCheck(i)) {
-                return i;
+        for (long i = 2; i <= num / 2; i++) {
+            if (num % i == 0 && utilities.isPrime.primeCheck(i) && i > max) {
+                max = i;
+                num /= i;
+                i = 2;
             }
         }
-        return -1;
+        if (utilities.isPrime.primeCheck(num) && num > max) {
+            max = num;
+        }
+        return max;
     }
 }
--- src/utilities/isPrime.java
+++ src/utilities/isPrime.java
@@ -2,10 +2,11 @@ package utilities;
 
 public class isPrime {
     public static boolean primeCheck(long n) {
-        if (n == 2 || n == 3) return true;
-        if (n <= 1 || n % 2 == 0 || n % 3 == 0) return false;
-        for (long i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0) return false;
+        if (n <= 1) return false;
+        long i = 2L;
+        while(i <= n / 2) {
+            if (n % i == 0) return false;
+            i++;
         }
         return true;
     }
