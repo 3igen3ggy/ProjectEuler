5942aca1a1133ec53db09ffc42e0871d14ff44f3
3igenKrak3n <3igen3ggy@gmail.com>
2022-02-23 19:26:20 +0100

    to debug, misses value by 10 (should be 997651)

---
+++ src/exercises/Ex50.java
@@ -0,0 +1,34 @@
+package exercises;
+
+import java.util.ArrayList;
+import static utilities.EratosthenesSieve.Sieve;
+
+public class Ex50 {
+    public static int solve() {
+        int n = 1000000;
+        int maxSum = 0;
+        int maxCounter = 0;
+        boolean[] sieve = Sieve(n);
+
+        ArrayList<Integer> primesList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (sieve[i]) primesList.add(i);
+        }
+        int len = primesList.size();
+
+        for (int i = 0; i < len; i++) {
+            int counter = 0;
+            int sum = 0;
+            for (int j = i; j < len; j++) {
+                sum += primesList.get(j);
+                counter++;
+                if (sum > n) break;
+                if (counter > maxCounter) {
+                    maxCounter = counter;
+                    maxSum = sum;
+                }
+            }
+        }
+        return maxSum;
+    }
+}
