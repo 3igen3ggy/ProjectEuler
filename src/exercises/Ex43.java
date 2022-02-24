583806f4f54113761e856cf631372c8477faa8d8
3igenKrak3n <3igen3ggy@gmail.com>
2022-02-24 20:49:36 +0100

    fixed 43

--- src/exercises/Ex43.java
+++ src/exercises/Ex43.java
@@ -1,56 +1,67 @@
 package exercises;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class Ex43 {
-    public static String solve() {
-        long max = 10000000000L;
-        int j = 3003840;
+    public static long solve() {
+        String num = "1234567890";
+        ArrayList<String> perms = permutation(num);
+        long sum = 0L;
 
-        BigInteger sum = new BigInteger("16695334890");
-
-        for (long i = 9248971292L; i < max; i++) {
-            System.out.println("j: " + j + ", i: " + i + ", sum: " + sum);
-            if (pandigit(i)) {
-                j++;
-                int d234 = createNum(digit(i, 1), digit(i, 2), digit(i, 3));
-                int d345 = createNum(digit(i, 2), digit(i, 3), digit(i, 4));
-                int d456 = createNum(digit(i, 3), digit(i, 4), digit(i, 5));
-                int d567 = createNum(digit(i, 4), digit(i, 5), digit(i, 6));
-                int d678 = createNum(digit(i, 5), digit(i, 6), digit(i, 7));
-                int d789 = createNum(digit(i, 6), digit(i, 7), digit(i, 8));
-                int d8910 = createNum(digit(i, 7), digit(i, 8), digit(i, 9));
-
-                if (d234 % 2 == 0 && d345 % 3 == 0 && d456 % 5 == 0 && d567 % 7 == 0 && d678 % 11 == 0 && d789 % 13 == 0 && d8910 % 17 == 0) {
-                    sum = sum.add(BigInteger.valueOf(i));
+        for (String s : perms) {
+            if (s.charAt(0) == '0') continue;
+            if (Integer.parseInt(s.substring(1, 4)) % 2 == 0) {
+                if (Integer.parseInt(s.substring(2, 5)) % 3 == 0) {
+                    if (Integer.parseInt(s.substring(3, 6)) % 5 == 0) {
+                        if (Integer.parseInt(s.substring(4, 7)) % 7 == 0) {
+                            if (Integer.parseInt(s.substring(5, 8)) % 11 == 0) {
+                                if (Integer.parseInt(s.substring(6, 9)) % 13 == 0) {
+                                    if (Integer.parseInt(s.substring(7, 10)) % 17 == 0) {
+                                        sum += Long.parseLong(s);
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
+
         }
-        return sum.toString();
+        return sum;
     }
 
-    public static int digit(long num, int d) {
-        String numberAux = Long.toString(num);
-        return Integer.parseInt(Character.toString(numberAux.charAt(d)));
+    //below is taken from https://stackoverflow.com/questions/4240080/generating-all-permutations-of-a-given-string
+    public static ArrayList<String> permutation(String s) {
+        // The result
+        ArrayList<String> res = new ArrayList<>();
+        // If input string's length is 1, return {s}
+        if (s.length() == 1) {
+            res.add(s);
+        } else if (s.length() > 1) {
+            int lastIndex = s.length() - 1;
+            // Find out the last character
+            String last = s.substring(lastIndex);
+            // Rest of the string
+            String rest = s.substring(0, lastIndex);
+            // Perform permutation on the rest string and
+            // merge with the last character
+            res = merge(permutation(rest), last);
+        }
+        return res;
     }
 
-    public static boolean pandigit (long num) {
-
-        boolean[] digits = new boolean[10];
-
-        for (int i = 0; i <= 9; i++) {
-            digits[digit(num, i)] = true;
-        }
-
-        for (int i = 0; i <= 9; i++) {
-            if (!digits[i]) {
-                return false;
+    public static ArrayList<String> merge(ArrayList<String> list, String c) {
+        ArrayList<String> res = new ArrayList<>();
+        // Loop through all the string in the list
+        for (String s : list) {
+            // For each string, insert the last character to all possible positions
+            // and add them to the new list
+            for (int i = 0; i <= s.length(); ++i) {
+                String ps = new StringBuffer(s).insert(i, c).toString();
+                res.add(ps);
             }
         }
-        return true;
-    }
-
-    public static int createNum(int a, int b, int c ) {
-        return a * 100 + b * 10 + c;
+        return res;
     }
 }
