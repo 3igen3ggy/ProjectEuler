b06b8861ea72bcbb57ea5a2f5449bcfc2f83832a
3igenKrak3n <3igen3ggy@gmail.com>
2022-02-24 20:50:00 +0100

    added pandigital test (with zero digit included)

--- src/test/isPandigitalTest.java
+++ src/test/isPandigitalTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import static utilities.isPandigital.isPandigit;
+import static utilities.isPandigital.isPandigitWithZero;
 
 public class isPandigitalTest {
     static int countPassed;
@@ -15,12 +16,31 @@ public class isPandigitalTest {
         test(124, false);
         test(1223, false);
         test(123456789, true);
+
+        testWithZero(0, true);
+        testWithZero(10, true);
+        testWithZero(120, true);
+        testWithZero(123, false);
+        testWithZero(654321, false);
+        testWithZero(76054321, true);
     }
 
     public static void test(int n, boolean pandigit) {
         totalTests++;
 
         if (isPandigit(n) == pandigit) {
+            countPassed++;
+            System.out.println("Test " + totalTests + " passed");
+        } else {
+            countFailed++;
+            System.out.println("Test " + totalTests + " NOT passed");
+        }
+    }
+
+    public static void testWithZero(int n, boolean pandigit) {
+        totalTests++;
+
+        if (isPandigitWithZero(n) == pandigit) {
             countPassed++;
             System.out.println("Test " + totalTests + " passed");
         } else {
--- src/utilities/isPandigital.java
+++ src/utilities/isPandigital.java
@@ -20,4 +20,24 @@ public class isPandigital {
         }
         return true;
     }
+
+    public static boolean isPandigitWithZero(int n) {
+        if (n == 0) return true;
+        int len = (int) Math.log10(n) + 1;
+        boolean[] digits = new boolean[len + 1];
+
+        for (int i = 0; i < len; i++) {
+            int digit = Integer.parseInt(String.valueOf(String.valueOf(n).charAt(i)));
+            try {
+                if (digits[digit]) return false;
+                digits[digit] = true;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return false;
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if (!digits[i]) return false;
+        }
+        return true;
+    }
 }
